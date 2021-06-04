eb02880d714f48364eb8a82f06267b29820cc8ad
Kamila Silva <kamillasilva140@gmail.com>
2021-06-04 17:29:23 -0300

    base de dados projeto livro

---
+++ database.js
@@ -0,0 +1,58 @@
+const livros = [
+
+ {
+    nome: 'O menino do pijama listrado',
+    autor: 'John Boyne',
+    categoria: 'Fábula',
+    paginas: 274,
+    leu: 'true',
+    recomenda: 'true'
+ },
+ {  nome: 'O pequeno principe preto',
+    autor: 'Rodrigo França',
+    categoria: 'Literatura infantil',
+    paginas: 32,
+    leu: 'true',
+    recomenda: 'true'
+ },
+ {  nome: 'Como eu era antes de você',
+    autor: 'Jojo Moyes',
+    categoria: 'Romance',
+    paginas: 320,
+    leu:'true',
+    recomenda: 'true'
+
+ },
+ {  nome: 'Pequeno manual antiracista',
+    autor: 'Djamila Ribeiro',
+    categoria:'Ciências humanas',
+    paginas: 136,
+    leu:'true',
+    recomenda:'true'
+ },
+ {  nome: 'Não se apega não',
+    autor: 'Isabela Freitas',
+    categoria: 'Autoajuda',
+    paginas: 256,
+    leu: 'true',
+    recomenda: 'true'
+ },
+ {  nome: 'Eu sei porque o passaro canta na gaiola',
+    autor: 'Angela Mayou',
+    categoria: 'Autobiografia',
+    paginas: 336,
+    leu: false,
+    recomenda: false
+
+ },
+ { nome: 'Para você que sente demais',
+   autor: 'Victor fernandes',
+   categoria: 'Ficção',
+   paginas: 176,
+   leu: false,
+   recomenda: false
+
+ }
+
+] 
+module.exports = livros
